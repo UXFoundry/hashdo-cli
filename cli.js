474d2f2d433e72f09b50cec3d72ebd8b3f5a00cd
Werner van Deventer <werner@brutaldev.com>
2015-09-24 23:53:07 +0200

    Updated to use hashdo-web to serve pages.

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 var Cli = module.exports,
-  Spawn = require('cross-spawn-async'),
+  HashDoWeb = require('hashdo-web'),
   Path = require('path'),
   Program = require('commander'),
   Open = require('open'),
@@ -15,48 +15,25 @@ Cli.run = function (processArgv) {
     console.log('/_  _  __/  / /_/ / / /_/ / ');
     console.log(' /_//_/    /_____/  \\____/ ');
     console.log('');
+    console.log('v%s', HashDoWeb.hashdo.version);
+    console.log('');
   }
 
   Program
     .command('serve')
     .description('Starts web server to render and test #Do cards.')
     .option('-p, --port <port>', 'Use an alternate port for the web server (defaults to 4000).', parseInt)
-    .action(function(options) {
+    .action(function (options) {
       printLogo();
       
-      var port = options.port || 4000;
-      var env = Object.create(process.env);
-            
-      env.NODE_ENV = 'development';
-      env.NODE_PATH=Path.join(__dirname, 'node_modules');
-      env.PORT = port;
-      env.CARDS_DIRECTORY = process.cwd();
-                        
-      var webServer = Spawn('node', ['app.js'], { 
-        cwd: __dirname, 
-        env: env
-      });
-      
-      webServer.stderr.on('data', function (data) {
-        console.error(data.toString());
-      });
-      
-      webServer.stdout.on('data', function (data) {
-        var output = _.trim(data.toString());
-        
-        console.log(output);
-        
-        // Bit hacky but this will do for now.
-        if (output === 'Let\'s #Do') {
-          Open('http://localhost:' + port);
-        }
-      });
+      var port = options.port || 4000;      
+      HashDoWeb.start('http://localhost:' + port, port, process.cwd());
     });
     
   Program
     .command('create <template>')
     .description('Creates a new #Do pack or adds a new card using a template generator.')
-    .action(function(template) {
+    .action(function (template) {
       YeomanEnv.register(Path.join(__dirname, 'yeoman/generator-hashdo/generators/app'), 'hashdo:pack');
       YeomanEnv.register(Path.join(__dirname, 'yeoman/generator-hashdo/generators/card'), 'hashdo:card');
       
